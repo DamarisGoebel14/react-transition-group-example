fbc4a098e4ba22ee41f7696ad7d24289850b1656
Damaris <d4maris@devisland.de>
2020-12-18 10:19:09 +0100

    example with css transition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react'
 import './App.css';
+import {CSSTransition} from "react-transition-group";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inProp, setInProp] = useState(false);
+
+       return (
+        <div>
+
+            <CSSTransition
+                in={inProp}
+                timeout={200}
+                classNames="my-node"
+                unmountOnExit
+
+            >
+                <div>
+                    {"I'll receive my-node-* classes"}
+                </div>
+            </CSSTransition>
+
+
+            <button type="button" onClick={() => setInProp(true)}>
+                Click to Enter
+            </button>
+
+            <button type="button" onClick={() => setInProp(false)}>
+                Click to Exit
+            </button>
+        </div>
+    );
 }
 
 export default App;
